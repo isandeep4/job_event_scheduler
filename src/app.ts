e8691157fa11c3b0b70fbad7b0888e8bac250de9
isandeep4 <isandeepsahoo5@gmail.com>
2024-08-07 16:57:13 -0600

    validated and sanitized inputs using express-validator

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
 
-import { createEventsRouter } from './events/router';
-import { Redis } from './redis/Redis';
+import { createEventsRouter } from "./events/router";
+import { Redis } from "./redis/Redis";
 
 async function main() {
-    const redis = await Redis.CreateAndConnect("redis://localhost:6379");
+  const redis = await Redis.CreateAndConnect("redis://localhost:6379");
 
-    const app = express();
-    app.use(bodyParser.text({limit: '1mb', type: 'application/json'}));
+  const app = express();
+  app.use(express.json());
 
-    app.use('/events', await createEventsRouter(redis));
+  app.use("/events", await createEventsRouter(redis));
 
-    const port = 4000;
-    app.listen(port, () => {
-        console.log('Web service started on port ' + port);
-    });
+  const port = 4000;
+  app.listen(port, () => {
+    console.log("Web service started on port " + port);
+  });
 }
 
 main();
--- src/events/router.ts
+++ src/events/router.ts
@@ -1,35 +1,70 @@
-import {Router} from "express";
+import { Router } from "express";
+import { body, validationResult } from "express-validator";
 import { IRedis } from "../redis/IRedis";
 import { EventCollector } from "./services/EventCollector";
 import { GetStatus } from "./services/GetStatus";
 
-export async function createEventsRouter(
-    redis: IRedis
-): Promise<Router> {
-    const router = Router({mergeParams: true});
-    const eventCollector = new EventCollector(redis);
-    const getStatus = new GetStatus(redis);
+export async function createEventsRouter(redis: IRedis): Promise<Router> {
+  const router = Router({ mergeParams: true });
+  const eventCollector = new EventCollector(redis);
+  const getStatus = new GetStatus(redis);
 
-    router
-        .post("/collect", async(req, res) => {
-            console.log("/events/collect");
+  const validateEvent = [
+    body("eventName").isString().withMessage("Event name must be a string"),
+    body("eventTimestamp").isInt().withMessage("Must be a valid timestamp"),
+    body("installId")
+      .isString()
+      .withMessage("install id must be string")
+      .notEmpty()
+      .withMessage("install id is required and can not be empty"),
+    body("sessionId")
+      .isString()
+      .withMessage("sessionId must be a string")
+      .isLength({ min: 3 })
+      .withMessage("Session id must be valid"),
+    body("platform")
+      .isString()
+      .withMessage("platform must be a string")
+      .notEmpty()
+      .withMessage("platform is required and can not be empty"),
+    body("version")
+      .isString()
+      .withMessage("version must be a string")
+      .notEmpty()
+      .withMessage("version is required and can not be empty"),
+    body("eventData")
+      .notEmpty()
+      .withMessage("eventData is required and can not be empty"),
+  ];
 
-            if(!req.headers || !req.headers['x-tactile-game-id']) {
-                return res.status(404).send({eventsCollected: 0});
-            }
+  router
+    .post("/collect", validateEvent, async (req, res) => {
+      console.log("/events/collect");
 
-            const game = req.headers['x-tactile-game-id'];
-            const eventsCollected = await eventCollector.collectEvent(game as string, req.body as string);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-            res.send({eventsCollected});
-        })
-        .get("/status", async(req, res) => {
-            console.log("/events/status");
+      if (!req.headers || !req.headers["x-tactile-game-id"]) {
+        return res.status(404).send({ eventsCollected: 0 });
+      }
 
-            const events = await getStatus.get();
+      const game = req.headers["x-tactile-game-id"];
+      const eventsCollected = await eventCollector.collectEvent(
+        game as string,
+        req.body as object
+      );
 
-            res.send(events);
-        });
+      res.send({ eventsCollected });
+    })
+    .get("/status", async (req, res) => {
+      console.log("/events/status");
 
-    return router;
+      const events = await getStatus.get();
+
+      res.send(events);
+    });
+
+  return router;
 }
